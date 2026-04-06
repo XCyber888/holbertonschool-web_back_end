9c74040ccab70caf7346965c07238fe258ccb27f
XCyber888 <xalidyusifov744@gmail.com>
2026-04-06 14:44:41 +0000

    Final fix for task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-"""
-Module that provides a type-annotated function add.
-"""
-
+""" Module docstring """
 
 def add(a: float, b: float) -> float:
-    """
-    Returns the sum of two floats as a float.
-    """
+    """ Function docstring """
     return a + b
