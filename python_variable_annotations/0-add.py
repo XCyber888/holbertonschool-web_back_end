d9519e1559996cf39970bade58a1eadb3a42b2ba
XCyber888 <xalidyusifov744@gmail.com>
2026-04-06 14:45:44 +0000

    Correct format for task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" Module docstring """
+"""Basic annotations - add"""
+
 
 def add(a: float, b: float) -> float:
-    """ Function docstring """
+    """Returns the sum of two floats"""
     return a + b
