a60e4a67ce5698d4bd090a2decce3c6e168a3506
XCyber888 <xalidyusifov744@gmail.com>
2026-04-08 06:46:23 +0000

    Fix requirements for task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module that provides an asynchronous coroutine wait_random.
+Module for async wait_random task.
 """
 import asyncio
 import random
@@ -8,7 +8,7 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Waits for a random delay between 0 and max_delay and returns it.
+    Waits for a random delay and returns it.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
