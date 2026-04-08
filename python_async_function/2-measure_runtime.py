ff091adac06c00238a55313f78b2b76c02d46b2b
XCyber888 <xalidyusifov744@gmail.com>
2026-04-08 06:49:22 +0000

    Fix: Ensure 5/5 score for task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -13,9 +13,8 @@ def measure_time(n: int, max_delay: int) -> float:
     Measures the total execution time for wait_n(n, max_delay)
     and returns total_time / n.
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.perf_counter()
-    
-    total_time = end_time - start_time
-    return total_time / n
+    end_time = time.time()
+
+    return (end_time - start_time) / n
