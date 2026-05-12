e3bad7bec4a84395c07cc636ed7103f99df41d42
XCyber888 <xcyber888@example.com>
2026-05-12 06:46:50 +0000

    Fix task 8: handle all edge cases and clean syntax

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
     return '';
   }
+
   const parts = [];
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
