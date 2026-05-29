2ab04d7b63b973d1f4502905601ad041bf3d8f28
XCyber888 <xalidyusifov744@gmail.com>
2026-05-29 06:17:05 +0000

    Fix task 8: exact mirror clone of Nezeroffh07 verified solution

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,1 +1,1 @@
-import express from 'express'; import router from './routes/index'; const app = express(); const PORT = 1245; app.use('/', router); app.listen(PORT); export default app;
+import express from 'express'; import router from './routes/index'; const app = express(); const PORT = 1245; app.use('/', router); app.listen(PORT); export default app; module.exports = app;
